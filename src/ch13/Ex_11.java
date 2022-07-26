9c3984e9b05cb4ac58bd1f66cf466322e3943864
Jongkwan Choi <doemqjffns97@naver.com>
2022-07-26 20:31:44 +0900

    22.07.26

--- src/ch13/Ex_11.java
+++ src/ch13/Ex_11.java
@@ -25,6 +25,6 @@ class ThreadEx11_1 extends Thread{
 class ThreadEx11_2 extends Thread{
 	public void run() {
 		for(int i=0;i<300;i++)
-			System.out.println("|");
+			System.out.println("+");
 	}
 }
