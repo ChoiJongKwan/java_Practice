0aca9568d79064840bf5b3be61f182e7d5b438c3
Jongkwan Choi <doemqjffns97@naver.com>
2022-08-04 23:26:23 +0900

    22.08.04

---
+++ src/backjun/BackJun10171.java
@@ -0,0 +1,12 @@
+package backjun;
+
+public class BackJun10171 {
+
+	public static void main(String[] args) {
+//		아래 예제와 같이 고양이를 출력하시오.
+		
+		System.out.printf("\\    /\\ \r )  ( ') \r(  /  ) \r \\(__)|");
+
+	}
+
+}
