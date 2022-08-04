34b9175c3d4f139ec0a30b14b968eb8565b19dce
Jongkwan Choi <doemqjffns97@naver.com>
2022-08-05 00:11:52 +0900

    백준 단계별로 풀어보기

---
+++ src/backjun/BackJun2884.java
@@ -0,0 +1,27 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class BackJun2884 {
+public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int A= sc.nextInt();
+		int B= sc.nextInt();
+
+		if(A>0&&B>=45)
+			System.out.println(A+" "+(B-45));
+		else if (A>0&&B<45) {
+			System.out.println(A-1+" "+(60-(45-B)));
+		}else if (A==0&&B<45) {
+			System.out.println(23+" "+(60-(45-B)));
+		}else if((A==0&&B>=45)){
+			System.out.println(A+" "+(B-45));
+		}else {
+			System.out.println("Error");
+		}
+			
+	
+	}
+}
