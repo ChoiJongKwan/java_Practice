80f01ba0224113f4886464cda8c9368bf1787960
Jongkwan Choi <doemqjffns97@naver.com>
2022-08-09 23:24:20 +0900

    2022.08.09

---
+++ src/Programmers/Main.java
@@ -0,0 +1,9 @@
+package Programmers;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/backjun/Main.java
+++ src/backjun/Main.java
@@ -3,32 +3,33 @@ package backjun;
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-	public static void main(String[] args)  {
 
-		Scanner sc =new Scanner(System.in);
-		int cnt=0;
-		String A= sc.next();
-		String B= A;
+
+	public static void main(String[] args) {
 		
-		if(A.length()<2)
-			B="0"+A;
-		
-		while(true){
-			int sum=0;
-			for(int i = 0; i<B.length(); i++ ) {
-				sum += B.charAt(i)-48;
-			}
-		B = B.charAt(1)+Integer.toString(sum%10);
-		cnt++;
-;
-		if(A.equals(B)||A.equals("0"+B)) break;
-		}
-		
-		System.out.print( cnt);
+		Solution sl = new Solution();
+		int num=0;
+		Scanner sc= new Scanner(System.in);
+		num =sc.nextInt();
+	
 
-
-
+		System.out.println(	sl.solution(num));
 	}
 
+
+
+
+	
+}
+class Solution {
+    public String solution(int num) {
+         String answer = "";
+        if(num%2==0)
+           answer = "Even";
+        else{
+            answer= "Odd";
+        }
+        return answer;
+    }
 
 }
